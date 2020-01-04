6d6b16027e5c520174d457c58042f0f690a7f27a
Andrew McOlash <amcolash@gmail.com>
2020-01-04 13:19:45 -0800

    reformat with prettier, no polling for spectrum

--- service-worker.js
+++ service-worker.js
@@ -2,49 +2,51 @@ var CACHE = 'remote v2';
 var SCOPE = self.registration.scope;
 
 var CACHE_LIST = [
-    SCOPE,
-    SCOPE + '/app.js',
-    SCOPE + '/axios.min.js',
-    SCOPE + '/feather.min.js',
-    SCOPE + '/index.html',
-    SCOPE + '/style.css'
+  SCOPE,
+  SCOPE + '/app.js',
+  SCOPE + '/axios.min.js',
+  SCOPE + '/feather.min.js',
+  SCOPE + '/index.html',
+  SCOPE + '/style.css'
 ];
 
 self.addEventListener('install', evt => {
-    console.log('The service worker is being installed.');
+  console.log('The service worker is being installed.');
 
-    // Precache and clean up old caches
-    evt.waitUntil(precache());
-    evt.waitUntil(cleanup());
+  // Precache and clean up old caches
+  evt.waitUntil(precache());
+  evt.waitUntil(cleanup());
 });
 
 // Cache most parts, but fetch from network if not availible locally
 self.addEventListener('fetch', evt => {
-    let request = evt.request;
+  let request = evt.request;
 
-    evt.respondWith(
-        caches.match(request).then(matching => {
-            return matching || fetch(request);
-        })
-    );
+  evt.respondWith(
+    caches.match(request).then(matching => {
+      return matching || fetch(request);
+    })
+  );
 });
 
 function precache() {
-    return caches.open(CACHE).then(cache => {
-        return cache.addAll(CACHE_LIST);
-    });
+  return caches.open(CACHE).then(cache => {
+    return cache.addAll(CACHE_LIST);
+  });
 }
 
 // Wipe no longer useful caches
 function cleanup() {
-    return caches.keys().then(cacheNames => {
-        return Promise.all(
-            cacheNames.filter(cacheName => {
-                return cacheName !== CACHE;
-            }).map(cacheName => {
-                console.log("Deleting old service worker cache", cacheName);
-                return caches.delete(cacheName);
-            })
-        );
-    });
+  return caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames
+        .filter(cacheName => {
+          return cacheName !== CACHE;
+        })
+        .map(cacheName => {
+          console.log('Deleting old service worker cache', cacheName);
+          return caches.delete(cacheName);
+        })
+    );
+  });
 }
